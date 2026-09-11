7bb6183c3743ba7faa43393898408cbcd94fca26
Elena2609 <lenaalexse@gmail.com>
2026-09-11 15:57:40 +0300

    Lab02: string to number conversion, Parse vs TryParse

--- obj/Debug/net10.0/Lab02-Alekseevskaia.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab02-Alekseevskaia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02-Alekseevskaia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6a0e40dd111f0780138e364dd6a065ae37d9b79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5243a1cbb356698c3b90450318bee1cb0d83cdcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02-Alekseevskaia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02-Alekseevskaia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
