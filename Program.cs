5243a1cbb356698c3b90450318bee1cb0d83cdcd
Elena2609 <lenaalexse@gmail.com>
2026-09-11 15:13:03 +0300

    Lab02: keyboard input with ReadLine

--- Program.cs
+++ Program.cs
@@ -46,3 +46,15 @@ var fullName = "Смирнова А.С";
 
 Console.WriteLine($"{fullName}, возраст {studentAge}, средний балл {gra}");
 
+Console.WriteLine();
+Console.WriteLine("Ввод текста");
+ 
+Console.Write("Введите ваше имя: ");
+string enteredName = Console.ReadLine();
+
+Console.Write("Введите название вашей группы: ");
+string enteredGroup = Console.ReadLine();
+
+Console.WriteLine($"Здравствуйте, {enteredName} из группы {enteredGroup}!");
+
+
